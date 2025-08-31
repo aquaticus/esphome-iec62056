8f6b9c6f7025b1bf93586f0dd49ba0376786909c
Vitalik33 <vitalik33@gmail.com>
2025-08-31 10:32:29 +0000

    iec62056: export CONF_IEC62056_ID/CONF_OBIS and add force_baud_rate & decrease_baud_on_retry to schema

--- components/iec62056/__init__.py
+++ components/iec62056/__init__.py
@@ -1,78 +1,63 @@
 # components/iec62056/__init__.py
-import re
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart
 from esphome.const import CONF_ID, CONF_UPDATE_INTERVAL
 
-AUTO_LOAD = ["sensor", "text_sensor"]
-DEPENDENCIES = ["uart"]
+AUTO_LOAD = ["uart"]
+MULTI_CONF = True
 
 iec62056_ns = cg.esphome_ns.namespace("iec62056")
-IEC62056Component = iec62056_ns.class_("IEC62056Component", cg.Component, uart.UARTDevice)
+IEC62056Component = iec62056_ns.class_(
+    "IEC62056Component", cg.PollingComponent, uart.UARTDevice
+)
 
-# ---- keys exported for sensor.py / text_sensor.py ----
+# ---- Константы/ключи, которые используют sensor.py и text_sensor.py ----
 CONF_IEC62056_ID = "iec62056_id"
 CONF_OBIS = "obis"
 
-# ---- component options ----
+# ---- Настройки компонента ----
 CONF_BAUD_RATE_MAX = "baud_rate_max"
-CONF_FORCE_BAUD_RATE = "force_baud_rate"
-CONF_DECREASE_BAUD_ON_RETRY = "decrease_baud_on_retry"
+CONF_FORCE_BAUD_RATE = "force_baud_rate"              # НОВОЕ
+CONF_DECREASE_BAUD_ON_RETRY = "decrease_baud_on_retry" # НОВОЕ
 CONF_BATTERY_METER = "battery_meter"
+CONF_CONNECTION_TIMEOUT = "connection_timeout"
 CONF_MAX_RETRIES = "max_retries"
 CONF_RETRY_DELAY = "retry_delay"
-CONF_RECEIVE_TIMEOUT = "receive_timeout"
-CONF_MODE_D = "mode_d"  # опционально, если используете принудит. Mode D
+CONF_MODE_D = "mode_d"
 
+# Простая проверка формата OBIS
 def validate_obis(value):
-    value = cv.string_strict(value)
-    # Примеры валидных кодов: 1.8.0, 2.6.0, 12.7.0, 15.8.1
-    if not re.fullmatch(r"\d+(?:\.\d+){2,3}", value):
-        raise cv.Invalid("OBIS должен выглядеть как '1.8.0', '2.6.0', '12.7.0' и т.п.")
-    return value
+    return cv.matches_regex(r"^\d+(?:\.\d+){2,}$")(value)
 
-CONFIG_SCHEMA = (
-    cv.Schema(
-        {
-            cv.GenerateID(CONF_IEC62056_ID): cv.declare_id(IEC62056Component),
-            cv.Optional(CONF_UPDATE_INTERVAL, default="67s"): cv.positive_time_period_milliseconds,
-            cv.Optional(CONF_BAUD_RATE_MAX, default=9600): cv.int_,
-            cv.Optional(CONF_FORCE_BAUD_RATE, default=0): cv.int_,  # 0 = авто по ID
-            cv.Optional(CONF_DECREASE_BAUD_ON_RETRY, default=True): cv.boolean,
-            cv.Optional(CONF_BATTERY_METER, default=False): cv.boolean,
-            cv.Optional(CONF_MAX_RETRIES, default=3): cv.int_range(min=0, max=10),
-            cv.Optional(CONF_RETRY_DELAY, default="3s"): cv.positive_time_period_milliseconds,
-            cv.Optional(CONF_RECEIVE_TIMEOUT, default="7s"): cv.positive_time_period_milliseconds,
-            cv.Optional(CONF_MODE_D, default=False): cv.boolean,
-        }
-    )
-    .extend(uart.UART_DEVICE_SCHEMA)
-    .extend(cv.COMPONENT_SCHEMA)
-)
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(IEC62056Component),
+        cv.Optional(CONF_UPDATE_INTERVAL, default="67s"): cv.positive_time_period_milliseconds,
+        cv.Optional(CONF_CONNECTION_TIMEOUT, default="7s"): cv.positive_time_period_milliseconds,
+        cv.Optional(CONF_BAUD_RATE_MAX, default=9600): cv.int_range(min=1200, max=9600),
+        cv.Optional(CONF_FORCE_BAUD_RATE, default=0): cv.int_range(min=0, max=9600),
+        cv.Optional(CONF_DECREASE_BAUD_ON_RETRY, default=True): cv.boolean,
+        cv.Optional(CONF_BATTERY_METER, default=False): cv.boolean,
+        cv.Optional(CONF_MAX_RETRIES, default=3): cv.uint8_t,
+        cv.Optional(CONF_RETRY_DELAY, default="3s"): cv.positive_time_period_milliseconds,
+        cv.Optional(CONF_MODE_D, default=False): cv.boolean,
+    }
+).extend(uart.UART_DEVICE_SCHEMA)
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_IEC62056_ID])
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
 
-    if CONF_UPDATE_INTERVAL in config:
-        cg.add(var.set_update_interval(config[CONF_UPDATE_INTERVAL].total_milliseconds))
-    if CONF_BAUD_RATE_MAX in config:
-        cg.add(var.set_baud_rate_max(config[CONF_BAUD_RATE_MAX]))
-    if CONF_RECEIVE_TIMEOUT in config:
-        cg.add(var.set_receive_timeout(config[CONF_RECEIVE_TIMEOUT].total_milliseconds))
-    if CONF_MAX_RETRIES in config:
-        cg.add(var.set_retry_counter_max(config[CONF_MAX_RETRIES]))
-    if CONF_RETRY_DELAY in config:
-        cg.add(var.set_retry_delay(config[CONF_RETRY_DELAY].total_milliseconds))
-    if CONF_BATTERY_METER in config:
-        cg.add(var.set_battery_meter(config[CONF_BATTERY_METER]))
-    if CONF_MODE_D in config:
-        cg.add(var.set_force_mode_d(config[CONF_MODE_D]))
+    cg.add(var.set_update_interval(config[CONF_UPDATE_INTERVAL].total_milliseconds))
+    cg.add(var.set_connection_timeout(config[CONF_CONNECTION_TIMEOUT].total_milliseconds))
+    cg.add(var.set_baud_rate_max(config[CONF_BAUD_RATE_MAX]))
+    cg.add(var.set_battery_meter(config[CONF_BATTERY_METER]))
+    cg.add(var.set_max_retries(config[CONF_MAX_RETRIES]))
+    cg.add(var.set_retry_delay(config[CONF_RETRY_DELAY].total_milliseconds))
+    cg.add(var.set_force_mode_d(config[CONF_MODE_D]))
 
     # Новые опции
-    if CONF_FORCE_BAUD_RATE in config:
-        cg.add(var.set_force_baud_rate(config[CONF_FORCE_BAUD_RATE]))
-    if CONF_DECREASE_BAUD_ON_RETRY in config:
-        cg.add(var.set_decrease_baud_on_retry(config[CONF_DECREASE_BAUD_ON_RETRY]))
+    cg.add(var.set_force_baud_rate(config[CONF_FORCE_BAUD_RATE]))
+    cg.add(var.set_decrease_baud_on_retry(config[CONF_DECREASE_BAUD_ON_RETRY]))
